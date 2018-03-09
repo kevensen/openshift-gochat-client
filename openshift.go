580b952d15d2f615802eb280232533f8e0cf84d4
Kenneth Evensen <kdevensen@gmail.com>
2018-03-09 19:16:20 +0000

    merge

--- openshift.go
+++ openshift.go
@@ -39,6 +39,7 @@ func (ocp *OpenShiftAuth) login(token string) (*User, error, int) {
 }
 
 func (ocp *OpenShiftAuth) get_project() string {
+
     glog.Infoln("Obtaining prject name from", ocp.apiHost+"/oapi/v1/projects/~")
 	resty.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 
