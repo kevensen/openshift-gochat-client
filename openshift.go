6829327cfa9c4c8b58831906ed4ce89d29ca63dc
Kenneth Evensen <kdevensen@gmail.com>
2018-03-09 19:24:03 +0000

    Still trying to fix the syntax error

--- openshift.go
+++ openshift.go
@@ -38,7 +38,7 @@ func (ocp *OpenShiftAuth) login(token string) (*User, error, int) {
 	return user, nil, resp.StatusCode()
 }
 
-func (ocp *OpenShiftAuth) get_project() (string {
+func (ocp *OpenShiftAuth) getProject() string {
 
     glog.Infoln("Obtaining prject name from", ocp.apiHost+"/oapi/v1/projects/~")
 	resty.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
